b1ddddd2f56bc24c907e0fa0f2519d720b323db4
Youngha Sung <youngha.sung@crl.com>
2022-03-30 11:11:51 -0500

    Clean up unnecessary code

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -3,9 +3,6 @@ import { Product } from '..'
 
 const ProductList = ({ products, compare, nameSearch }) => {
   const renderProducts = () => {
-    // The input search field is not functioning like expected.
-    // Fix it so that it can filter the rendered product list by name.
-    // If we type 3 characters it should search locally and return only the matching products.
     return products;
   }
 
